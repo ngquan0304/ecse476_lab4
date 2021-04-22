a1e91846a5ac2335e38b6189910fe32efe559433
Quan Nguyen <ngquan0304@gmail.com>
2021-04-22 13:18:01 -0400

    Created find_block node pub block_frame

--- table_transform/src/find_block.cpp
+++ table_transform/src/find_block.cpp
@@ -37,7 +37,7 @@ void find_indices_of_plane_from_patch(pcl::PointCloud<pcl::PointXYZRGB>::Ptr inp
     pcl::PassThrough<pcl::PointXYZRGB> pass; //create a pass-through object
     pass.setInputCloud(input_cloud_ptr);     //set the cloud we want to operate on--pass via a pointer
     pass.setFilterFieldName("z");            // we will "filter" based on points that lie within some range of z-value
-    pass.setFilterLimits(0.01, 0.05);        //retain points with z values between these limits
+    pass.setFilterLimits(0.02, 0.05);        //retain points with z values between these limits
     pass.filter(indices);                    //  this will return the indices of the points in given cloud that pass our test
     cout << "number of points passing the filter = " << indices.size() << endl;
     //This fnc populates the reference arg "indices", so the calling fnc gets the list of interesting points
@@ -95,6 +95,11 @@ int main(int argc, char **argv)
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclKinect_clr_ptr(new pcl::PointCloud<pcl::PointXYZRGB>); //pointer for color version of pointcloud
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr output_cloud_wrt_table_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr pts_above_table_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pts_above_right_table_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointXYZRGB block_centroid;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
 
     //load a PCD file using pcl::io function; alternatively, could subscribe to Kinect messages
     string fname;
@@ -113,8 +118,9 @@ int main(int argc, char **argv)
     ros::Publisher pubCloud = nh.advertise<sensor_msgs::PointCloud2>("/pcd", 1);
     ros::Publisher pubTableFrame = nh.advertise<sensor_msgs::PointCloud2>("table_frame_pts", 1);
     ros::Publisher pubPointsAboveTable = nh.advertise<sensor_msgs::PointCloud2>("pts_above_table", 1);
+    ros::Publisher pubPointsAboveRightTable = nh.advertise<sensor_msgs::PointCloud2>("pts_above_right_table", 1);
 
-    sensor_msgs::PointCloud2 ros_cloud_wrt_table, ros_pts_above_table, ros_cloud_orig;
+    sensor_msgs::PointCloud2 ros_cloud_wrt_table, ros_pts_above_table, ros_cloud_orig, ros_pts_above_right_table;
     //ros_cloud_wrt_table.header.frame_id = "table_frame";
 
     pcl::toROSMsg(*pclKinect_clr_ptr, ros_cloud_orig); //convert from PCL cloud to ROS message this way
@@ -138,11 +144,40 @@ int main(int argc, char **argv)
     pcl::toROSMsg(*pts_above_table_ptr, ros_pts_above_table);
     ros_pts_above_table.header.frame_id = "table_frame";
 
+    Eigen::Vector3f box_pt_min, box_pt_max;
+    box_pt_min << 0, -1, -1;
+    box_pt_max << 1, 0.2, 1;
+
+    vector<int> indices2;
+    pclUtils.box_filter(pts_above_table_ptr, box_pt_min, box_pt_max, indices);
+    pcl::copyPointCloud(*pts_above_table_ptr, indices, *pts_above_right_table_ptr); //extract these pts into new cloud
+    pcl::toROSMsg(*pts_above_right_table_ptr, ros_pts_above_right_table); //convert to ros message for publication and display
+    ros_pts_above_right_table.header.frame_id = "table_frame";
+    
+    // Reduce the height of the block by half for the correct centroid calculation
+    int pts_block_size = pts_above_right_table_ptr->points.size();
+    for (int i=0; i<pts_block_size; i++)
+    {
+        pts_above_right_table_ptr->points[i].z = (pts_above_right_table_ptr->points[i].z)/2;
+    }
+
+    pcl::computeCentroid(*pts_above_right_table_ptr, indices, block_centroid);
+
+    float block_x = block_centroid.x;
+    float block_y = block_centroid.y;
+    float block_z = block_centroid.z;
+
     while (ros::ok())
     {
         pubTableFrame.publish(ros_cloud_wrt_table);
         pubCloud.publish(ros_cloud_orig); // will not need to keep republishing if display setting is persistent
         pubPointsAboveTable.publish(ros_pts_above_table);
+        pubPointsAboveRightTable.publish(ros_pts_above_right_table);
+
+        transform.setOrigin(tf::Vector3(block_x, block_y, block_z));
+        transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"table_frame", "block_frame"));
+
         ros::spinOnce(); //pclUtils needs some spin cycles to invoke callbacks for new selected points
         ros::Duration(0.3).sleep();
     }
